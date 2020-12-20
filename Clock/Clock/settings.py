e408578895f82232360eb3a29edcfb4d81477237
Gautam Sharma <2019pietcsgautam63@poornima.org>
2020-12-20 08:58:57 +0530

    Update
    Solve Some Errors

--- Clock/Clock/settings.py
+++ Clock/Clock/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'lh9z@xu%grds*kp-)aem^38ef2d!74ahz1g+vp&vum+(!!4@@='
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
